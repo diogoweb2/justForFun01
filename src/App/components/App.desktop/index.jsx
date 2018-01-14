8b78652af721d45074848328e3959586fd7cb7ec
Diogo Lopes <diogo.lopes@bgrs.com>
2018-01-13 21:31:47 -0500

    delete folder

--- src/App/components/App.desktop/index.jsx
+++
@@ -1,9 +0,0 @@
-import { connect } from "react-redux";
-import DesktopView from "./component";
-
-const mapStateToProps = state => ({
-  selectedItem: state.messagesReducer.selectedItem,
-  dataLoaded: state.messagesReducer.dataLoaded
-});
-
-export default connect(mapStateToProps)(DesktopView);
